ea89bcf55746d15ae1705ae0fb542a479d5f2c6e
J. Matt Peterson <jmatt@jmatt.org>
2015-10-19 17:18:56 -0700

    [DM-4106] Add credential caching to reduce GitHub API calls.
        * Use redis + scrypt to safely cache username + password to reduce
          the number of GitHub API calls.
    
    	modified:   git-lfs-s3-server.rb

--- git-lfs-s3-server.rb
+++ git-lfs-s3-server.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
-require 'rubygems'
+# require 'rubygems'
 require 'logger'
 require 'git-lfs-s3'
 require 'octokit'
+require 'redis'
+require 'scrypt'
 
+# Configure lsst-git-lfs-s3 gem.
 GitLfsS3::Application.set :aws_region, ENV['AWS_REGION']
 GitLfsS3::Application.set :aws_access_key_id, ENV['AWS_ACCESS_KEY_ID']
 GitLfsS3::Application.set :aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY']
@@ -14,6 +17,18 @@ GitLfsS3::Application.set :ceph_s3, (ENV['LFS_CEPH_S3'] == 'true')
 GitLfsS3::Application.set :endpoint, ENV['LFS_CEPH_ENDPOINT']
 GitLfsS3::Application.set :logger, Logger.new(STDOUT)
 
+# GitHub Organization used to verify membership.
+GITHUB_ORG = 'lsst'
+
+# Configure redis.
+@redis = Redis.new
+# Seconds to cache valid authentication.
+CACHE_EXPIRE = 900
+
+# Configure scrypt for caching.
+SCrypt::Engine.calibrate
+
+# Configure aws-sdk for Ceph S3.
 if GitLfsS3::Application.settings.ceph_s3
   if not GitLfsS3::Application.settings.public_server
     raise 'Ceph S3 only supports public_server mode.'
@@ -31,12 +46,10 @@ if GitLfsS3::Application.settings.ceph_s3
 )
 end
 
-def verify_user_and_permissions(username, password)
+def org_member?(client)
   begin
-    client = Octokit::Client.new(:login => username,
-                                 :password => password)
-    client.user
-    if client.org_member?('lsst', client.user.login)
+    client.user # Authenticate User.
+    if client.org_member?(GITHUB_ORG, client.user.login)
       return true
     else
       return false
@@ -50,10 +63,42 @@ def verify_user_and_permissions(username, password)
   end
 end
 
+def verify_user_and_permissions?(client, username, password)
+  result = false
+  begin
+    # Use the request username to avoid using the github API.
+    if @redis.connected?
+      cached_hash = @redis.get(username)
+    else
+      cached_hash = nil
+    end
+    if cached_hash
+      cached_password = SCrypt::Password.new(cached_hash)
+      result = cached_password == password
+    else
+      if org_member?(client)
+        result = true
+        hash = SCrypt::Password.create(
+          password,
+          salt: SCrypt::Engine.generate_salt)
+        @redis.set(client.user.login, hash, ex: CACHE_EXPIRE)
+      else
+        result = false
+      end
+    end
+  rescue Redis::BaseConnectionError => e
+    GitLfsS3::Application.settings.logger.warn\
+      e.message
+  end
+  return result
+end
+
 GitLfsS3::Application.on_authenticate do |username, password, is_safe|
   if is_safe
     true
   else
-    verify_user_and_permissions(username, password)
+    client = Octokit::Client.new(:login => username,
+                                 :password => password)
+    verify_user_and_permissions?(client, username, password)
   end
 end
