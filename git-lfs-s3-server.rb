41ba1fcf33816debb3edb1a446dbf90fd482fcbe
Joshua Hoblitt <josh@hoblitt.com>
2019-01-04 09:07:11 -0700

    rm unused s3 "backup" functionality

--- git-lfs-s3-server.rb
+++ git-lfs-s3-server.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'after_do'
 require 'logger'
 require 'git-lfs-s3'
 require 'multi_json'
@@ -118,54 +117,4 @@ GitLfsS3::Application.on_authenticate do |username, password, is_safe|
   client = Octokit::Client.new(login:    username,
                                password: password)
   verify_user_and_permissions?(client, username, password)
-end
-
-####
-# Notify Backup Service
-####
-
-# Auth method pulled out of GitLfsS3::Application internals.
-# So the AfterDo block can auth outside of the lsst-git-lfs-s3 library.
-def authorized?(env, app)
-  auth = Rack::Auth::Basic::Request.new(env)
-  auth.provided? &&
-    auth.basic? &&
-    auth.credentials &&
-    app.class.auth_callback.call(
-      auth.credentials[0], auth.credentials[1], false
-    )
-end
-
-def verify_call?(env, app, req)
-  req.post? &&
-    (req.path == '/verify') &&
-    req.content_type.include?('application/vnd.git-lfs+json') &&
-    authorized?(env, app)
-end
-
-# AfterDo is a library that allows simple callbacks to methods.
-# After sinatra's base call method this block is called.
-GitLfsS3::Application.extend AfterDo
-GitLfsS3::Application.after :call do |env, app|
-  # Create a request
-  req = Rack::Request.new(env)
-  # Check the HTTP method, route, content_type and whether it's authorized.
-  if verify_call? env, app, req
-    data = MultiJson.load(req.body.tap(&:rewind).read)
-    oid = data['oid']
-    # Use the s3 object/key name.
-    oid_s3_name = 'data/' + oid
-    if @redis.connected?
-      # Check to see if backup already exists.
-      unless @redis.get('backup=>' + oid_s3_name)
-        @redis.publish 'backup', oid_s3_name
-        # Log publish message.
-        GitLfsS3::Application.settings.logger.debug(
-          "Publish message to backup S3 object #{oid_s3_name}."
-        )
-      end
-    else
-      GitLfsS3::Application.settings.logger.warn "Unable to backup oid = #{oid}"
-    end
-  end
 end
